e1e003dde055f573505e6a5266936100d60a6e32
vkev <khanghv2406@gmail.com>
2025-11-29 13:45:40 +0700

    Update

--- Terraform/main_eks.tf
+++ Terraform/main_eks.tf
@@ -66,7 +66,7 @@ locals {
     keys(local.rds_placeholder_map),
     local.eks_microservices_content,
     # Replace each TERRAFORM_RDS_* placeholder with its actual value from Terraform outputs
-    lambda(acc, k, replace(acc, k, local.rds_placeholder_map[k]))
+    lambda(acc, k, replace(acc, k, lookup(local.rds_placeholder_map, k, k)))
   ) : ""
 }
 
